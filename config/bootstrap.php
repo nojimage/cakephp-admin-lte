c73993c829394917aca8baa73be6979415666f44
nojimage <nojimage@gmail.com>
2015-05-29 21:16:09 +0900

    add horizontal form

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -33,3 +33,16 @@ Configure::write('AdminLTE.searchFormOptions', [
         'label' => '<span class="input-group-btn pull-left"><button class="btn btn-default"><i class="fa fa-search"></i></button></span>',
     ]
 ]);
+
+Configure::write('AdminLTE.formHorizontalOptions', [
+    'class' => 'form-horizontal',
+    'templates' => [
+        'checkbox' => '<input type="checkbox" name="{{name}}" value="{{value}}"{{attrs}}> ',
+        'radio' => '<input type="radio" name="{{name}}" value="{{value}}"{{attrs}}> ',
+        'error' => '<p class="help-block error-message text-red">{{content}}</p>',
+        'formGroup' => '{{label}}<div class="col-sm-9">{{input}}</div>',
+        'input' => '<input type="{{type}}" name="{{name}}"{{attrs}}>',
+        'inputContainer' => '<div class="form-group {{type}}{{required}}">{{content}}</div>',
+        'inputContainerError' => '<div class="form-group {{type}}{{required}} has-error">{{content}}{{error}}</div>',
+    ],
+]);
